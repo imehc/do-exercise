34f1d1bbc613e0966e506018c6596ed9ad98f014
imehc <richohealthy@hotmail.com>
2025-01-24 12:02:42 +0800

    feat(web): sidebar

--- web/app/[locale]/dashboard/layout.tsx
+++ web/app/[locale]/dashboard/layout.tsx
@@ -1,3 +1,18 @@
-import AuthLayout from '~/components/common/header';
+import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar';
+import { AppSidebar } from '~/components/other/app-sidebar';
 
-export default AuthLayout;
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function DashboardLayout({ children }: LayoutProps) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}

840358a0506fd33f9c9048251c83358141f153a1
imehc <richohealthy@hotmail.com>
2025-02-12 16:34:38 +0800

    feat(server): 上下文优先使用缓存

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -1,10 +1,13 @@
 package middleware
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 	"github.com/imehc/do-exercise/server/global"
 	"github.com/imehc/do-exercise/server/model/common/response"
 	"github.com/imehc/do-exercise/server/model/system"
+	"github.com/imehc/do-exercise/server/pkg/utils"
 )
 
 func JWTAuth() gin.HandlerFunc {
@@ -15,27 +18,44 @@ func JWTAuth() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
-		claims := system.Claims{}
+		var claims system.Claims
 		username := token.GetUserID()
 		if username == "" {
 			response.Unauthorized(c)
 			c.Abort()
 			return
 		}
-		// TODO: 第一次查询使用数据库后面使用缓存
+
+		log := global.LOG
 		db := global.DB
+		cache := global.Cache
 		var user system.User
-		err = db.Model(&system.User{}).Where("username = ?", username).First(&user).Error
-		if err != nil {
-			response.Unauthorized(c)
-			c.Abort()
-			return
+		claimsCache, exist := cache.Get(global.CLAIMS)
+		if exist {
+			_ = json.Unmarshal([]byte(claimsCache.(string)), &claims)
+			log.Info("Use cache")
+		} else {
+			err = db.Model(&system.User{}).Where("username = ?", username).First(&user).Error
+			if err != nil {
+				response.Unauthorized(c)
+				c.Abort()
+				return
+			}
+			claims.ID = user.ID
+			claims.Username = user.Username
+			claims.DeptId = user.DeptId
+			claims.PostId = user.PostId
+			claims.RoleId = user.RoleId
+
+			expire, err := utils.ParseDurationString(global.CONFIG.Auth.AccessExpire)
+			if err == nil {
+				jsonData, err := json.Marshal(claims)
+				if err == nil {
+					cache.Set(global.CLAIMS, jsonData, expire)
+				}
+			}
+			log.Info("Use database")
 		}
-		claims.ID = user.ID
-		claims.Username = user.Username
-		claims.DeptId = user.DeptId
-		claims.PostId = user.PostId
-		claims.RoleId = user.RoleId
 
 		// TODO: 首先从redis中查出对应信息，如果redis中没有，则从数据库查出对应信息，存入上下文
 		// TODO: 从redis中查出对应信息，比如租户ID,存入上下文, 住户解析验证租户是否存在，租户信息是否缺失

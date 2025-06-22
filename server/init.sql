ca09035bd16daa1094510ee6b853bbd3eb4bde33
imehc <richohealthy@hotmail.com>
2025-06-22 20:27:49 +0800

    fix(server): 修正API插入顺序并更新ID以确保唯一性
    - 调整了`init.sql`中的API插入顺序，确保ID的唯一性
    - 更新了邮箱登录、验证码发送、刷新令牌等相关API的ID
    - 确保数据库初始化脚本的正确性和一致性

--- server/init.sql
+++ server/init.sql
@@ -39,17 +39,17 @@ INSERT INTO sys_api VALUES(108, '/user/modify_password', '用户更新密码', '
 INSERT INTO sys_api VALUES(109, '/auth/public_key', '获取加密公钥', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
 INSERT INTO sys_api VALUES(110, '/auth/captcha', '获取图形验证码', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
 INSERT INTO sys_api VALUES(111, '/auth/login', '登录', 'COMMON', 'POST', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(111, '/auth/login_with_email', '邮箱登录', 'COMMON', 'POST', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(111, '/auth/code_with_email', '发送邮箱登录验证码', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(112, '/auth/refresh_token', '刷新令牌', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(113, '/auth/forget_password_code', '发送忘记密码验证码', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(114, '/auth/forget_password', '忘记密码', 'COMMON', 'PATCH', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(115, '/auth/logout', '退出登录', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(112, '/auth/login_with_email', '邮箱登录', 'COMMON', 'POST', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(113, '/auth/code_with_email', '发送邮箱登录验证码', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(114, '/auth/refresh_token', '刷新令牌', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(115, '/auth/forget_password_code', '发送忘记密码验证码', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(116, '/auth/forget_password', '忘记密码', 'COMMON', 'PATCH', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(117, '/auth/logout', '退出登录', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
 
-INSERT INTO sys_api VALUES(116, '/get_presigned_url', '获取预上传文件地址', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
-INSERT INTO sys_api VALUES(117, '/sse', 'SSE消息通知', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(118, '/get_presigned_url', '获取预上传文件地址', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(119, '/sse', 'SSE消息通知', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
 
-INSERT INTO sys_api VALUES(118, '/health', '获取服务器状态', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
+INSERT INTO sys_api VALUES(120, '/health', '获取服务器状态', 'COMMON', 'GET', true, 0, '2025-03-06 08:19:00.621', '2025-03-06 08:19:00.621', NULL);
 
 SELECT setval(pg_get_serial_sequence('sys_api', 'id'), (SELECT MAX(id) FROM sys_api)); -- 重置自增序列;
 

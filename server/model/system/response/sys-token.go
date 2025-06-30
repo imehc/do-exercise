b5ca0f3096b2b183d8ec5f61f5e2938f27dcb914
imehc <richohealthy@hotmail.com>
2025-06-30 10:43:36 +0800

    feat(server): 增加令牌创建和过期时间的详细记录
    添加对access token和refresh token创建及过期时间的分别记录
    更新相关数据结构和服务逻辑以支持新字段

--- server/model/system/response/sys-token.go
+++ server/model/system/response/sys-token.go
@@ -3,11 +3,13 @@ package response
 import "time"
 
 type SysTokenLogRsp struct {
-	UserId       string    `json:"user_id"`
-	Username     string    `json:"username"`
-	AccessToken  string    `json:"access_token"`
-	RefreshToken string    `json:"refresh_token"`
-	Disabled     bool      `json:"disabled"`
-	CreatedAt    time.Time `json:"created_at"`
-	ExpiredAt    time.Time `json:"expired_at"`
+	UserId              string    `json:"user_id"`
+	Username            string    `json:"username"`
+	AccessToken         string    `json:"access_token"`
+	RefreshToken        string    `json:"refresh_token"`
+	Disabled            bool      `json:"disabled"`
+	AccessTokenCreated  time.Time `json:"access_token_created"`
+	AccessTokenExpired  time.Time `json:"access_token_expired"`
+	RefreshTokenCreated time.Time `json:"refresh_token_created"`
+	RefreshTokenExpired time.Time `json:"refresh_token_expired"`
 }
